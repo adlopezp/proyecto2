f9a556bbd580131d2fbdb23a93c666c5f80ce6e5
Zamir García <zgarcia83@gmail.com>
2015-07-20 23:47:16 -0500

    administración de archivos

--- Proyecto2/src/main/java/co/edu/uniandes/statusquo/operador/bean/DocumentView.java
+++ Proyecto2/src/main/java/co/edu/uniandes/statusquo/operador/bean/DocumentView.java
@@ -1,17 +1,23 @@
 package co.edu.uniandes.statusquo.operador.bean;
 
+import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 
+import org.primefaces.event.NodeSelectEvent;
 import org.primefaces.model.TreeNode;
 
+import co.edu.uniandes.statusquo.operador.business.Document;
 import co.edu.uniandes.statusquo.operador.services.DocumentService;
  
-@ManagedBean(name="treeIconView")
-public class IconView {
+@ManagedBean(name="documentView")
+public class DocumentView {
      
     private TreeNode root;
+    
+    private List<Document> currentDocuments;
      
     @ManagedProperty("#{documentService}")
     private DocumentService service;
@@ -28,4 +34,17 @@ public class IconView {
     public TreeNode getRoot() {
         return root;
     }
+    
+	public List<Document> getCurrentDocuments() {
+		return currentDocuments;
+	}
+
+	public void setCurrentDocuments(List<Document> currentDocuments) {
+		this.currentDocuments = currentDocuments;
+	}
+
+	public void onNodeSelect(NodeSelectEvent event) {
+		currentDocuments = ((Document) event.getTreeNode().getData()).getListDocuments();
+    }
+
 }
--- Proyecto2/src/main/java/co/edu/uniandes/statusquo/operador/business/Document.java
+++ Proyecto2/src/main/java/co/edu/uniandes/statusquo/operador/business/Document.java
@@ -1,94 +1,194 @@
 package co.edu.uniandes.statusquo.operador.business;
 
-import java.io.Serializable;
+import java.util.List;
 
-public class Document implements Serializable, Comparable<Document> {
- 
-    /**
-	 * 
+
+
+/**
+ * Representa un documento o directorio en el sistema
+ * @author Zamir
+ * @version 1.0
+ */
+public class Document {
+	
+	public enum Type {
+		FOLDER, FILE
+	}
+
+	/**
+	 * Identificador del archivo o direcotorio
 	 */
-	private static final long serialVersionUID = 1723638492644436198L;
-
+	private long id;
+	
+	/**
+	 * Nombre del archivo
+	 */
 	private String name;
-     
-    private String size;
-     
-    private String type;
-     
-    public Document(String name, String size, String type) {
-        this.name = name;
-        this.size = size;
-        this.type = type;
-    }
- 
-    public String getName() {
-        return name;
-    }
- 
-    public void setName(String name) {
-        this.name = name;
-    }
- 
-    public String getSize() {
-        return size;
-    }
- 
-    public void setSize(String size) {
-        this.size = size;
-    }
- 
-    public String getType() {
-        return type;
-    }
- 
-    public void setType(String type) {
-        this.type = type;
-    }
- 
-    //Eclipse Generated hashCode and equals
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((size == null) ? 0 : size.hashCode());
-        result = prime * result + ((type == null) ? 0 : type.hashCode());
-        return result;
-    }
- 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Document other = (Document) obj;
-        if (name == null) {
-            if (other.name != null)
-                return false;
-        } else if (!name.equals(other.name))
-            return false;
-        if (size == null) {
-            if (other.size != null)
-                return false;
-        } else if (!size.equals(other.size))
-            return false;
-        if (type == null) {
-            if (other.type != null)
-                return false;
-        } else if (!type.equals(other.type))
-            return false;
-        return true;
-    }
- 
-    @Override
-    public String toString() {
-        return name;
-    }
- 
-    public int compareTo(Document document) {
-        return this.getName().compareTo(document.getName());
-    }
-}  
+	
+	/**
+	 * Tamaño en bytes del archivo
+	 */
+	private long size;
+	
+	/**
+	 * Tipo de archivo
+	 */
+	private Type type;
+	
+	/**
+	 * Indica si el documento está firmado digitalmente
+	 */
+	private boolean signed;
+	
+	/**
+	 * Directorio al cual pertenece
+	 */
+	private Document parent;
+	
+	/**
+	 * Si es un directorio, esta lista contiene los archivos y directorios
+	 * a que haya lugar
+	 */
+	private List<Document> listDocuments;
+	
+	/**
+	 * Metadatos del documento
+	 */
+	private DocumentMetaData metadata;
+	
+	/**
+	 * Información sobre la manera en que se comparte el documento
+	 */
+	private List<ShareInfo> shareInfo;
+
+	public Document(){
+		type = Type.FILE;
+	}
+	
+	public Document(long id, String name, long size, Type type,
+			boolean signed, List<Document> listDocuments, DocumentMetaData metadata,
+			List<ShareInfo> shareInfo) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.size = size;
+		this.type = type;
+		this.signed = signed;
+		this.listDocuments = listDocuments;
+		this.metadata = metadata;
+		this.shareInfo = shareInfo;
+	}
+
+
+
+	public Document(String name, String string2, String type) {
+		this.name = name;
+	}
+
+	public Document(long id, String name, Type type) {
+		this.id = id;
+		this.name = name;
+		this.type = type;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getSize() {
+		return size;
+	}
+
+	public void setSize(long size) {
+		this.size = size;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public boolean isSigned() {
+		return signed;
+	}
+
+	public void setSigned(boolean signed) {
+		this.signed = signed;
+	}
+
+	public Document getParent() {
+		return parent;
+	}
+
+	public void setParent(Document parent) {
+		this.parent = parent;
+	}
+
+	public List<Document> getListDocuments() {
+		return listDocuments;
+	}
+
+	public void setListDocuments(List<Document> listDocuments) {
+		this.listDocuments = listDocuments;
+	}
+
+	public DocumentMetaData getMetadata() {
+		return metadata;
+	}
+
+	public void setMetadata(DocumentMetaData metadata) {
+		this.metadata = metadata;
+	}
+
+	public List<ShareInfo> getShareInfo() {
+		return shareInfo;
+	}
+
+	public void setShareInfo(List<ShareInfo> shareInfo) {
+		this.shareInfo = shareInfo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Document other = (Document) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Document [id=" + id + ", name=" + name + "]";
+	}
+	
+
+}
