2a7133ca953644ccc855bae9e2e3fa6eb05c175e
Zamir García <zgarcia83@gmail.com>
2015-07-21 01:40:56 -0500

    Se adiciona carga de archivos usando drag and drop

---
+++ Proyecto2/src/main/java/co/edu/uniandes/statusquo/operador/bean/UploadBean.java
@@ -0,0 +1,16 @@
+package co.edu.uniandes.statusquo.operador.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.FileUploadEvent;
+ 
+@ManagedBean
+public class UploadBean {
+ 
+    public void handleFileUpload(FileUploadEvent event) {
+        FacesMessage message = new FacesMessage("Succesful", event.getFile().getFileName() + " is uploaded.");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+}
